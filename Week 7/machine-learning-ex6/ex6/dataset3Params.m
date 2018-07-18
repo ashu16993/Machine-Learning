667a7bdd4891160502822c0bb5f60e3b6fc867af
ashu16993 <ashu1693@gmail.com>
2018-07-18 08:55:27 +0530

    Implemented dataset3Params
    Implmented code to choose the best possible C and sigma using cross-validation set

--- Week 7/machine-learning-ex6/ex6/dataset3Params.m
+++ Week 7/machine-learning-ex6/ex6/dataset3Params.m
@@ -22,8 +22,29 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_sigma_choices = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+error =  zeros(size(C_sigma_choices), size(C_sigma_choices));
 
+n = size(C_sigma_choices);
+n = size(C_sigma_choices);
 
+for i = 1:n
+  for j = 1:n
+    prediction = zeros(size(yval));
+    model= svmTrain(X, y, C_sigma_choices(i),...
+    @(x1, x2) gaussianKernel(x1, x2, C_sigma_choices(j))); 
+    prediction = svmPredict(model,Xval);
+    error(i,j) = mean(double(prediction ~= yval));
+  endfor
+endfor
+
+[vector_val vector_pos]=min(error)
+[val pos] = min(vector_val)
+
+##C = C_sigma_choices(vector_pos(1))
+sigma = C_sigma_choices(pos)
+
+C = C_sigma_choices(vector_pos(pos))
 
 
 
